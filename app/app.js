1da5e86208f3b242cc3ff5e304f5ca35b2fb51fb
Mark Winteringham <mark.winteringham@digital.hmrc.gov.uk>
2015-03-16 16:42:19 +0000

    Added in sqlite db for database events]

--- app/app.js
+++ app/app.js
@@ -2,7 +2,9 @@ var express    = require('express'),
     bodyParser = require('body-parser'),
     log4js     = require('log4js');
     fs         = require('fs');
+    sqlite3 = require('sqlite3').verbose();
 
+var db = new sqlite3.Database('events.sqlt');
 var logger = log4js.getLogger(),
     app = express();
 
@@ -21,16 +23,28 @@ log4js.configure({
 // Endpoints to turn on and off the app randomisation
 
 app.get('/start',function(req,res){
-  logger.debug('GET /start - Starting app');
+  console.log('########## EVENT GENERATOR - Starting app ##########');
   lock = true;
   dotCounter();
   res.sendStatus(200);
 })
 
 app.get('/stop',function(req,res){
-  logger.debug('GET /stop - Stopping app');
+  console.log('########## EVENT GENERATOR - Stopping app ##########');
   lock = false;
   res.sendStatus(200);
+})
+
+app.get('/purge',function(req,res){
+  console.log('########## EVENT GENERATOR - Purging db and logs ##########')
+  fs.writeFile('logs/eventgenerator.log', '', function (err) {
+    if (err) return console.log(err);
+
+    db.run("DELETE FROM events", function (err){
+      if (err) return console.log(err);
+      res.sendStatus(200);
+    });
+  });
 })
 
 // Start the app up and console out app usage
@@ -59,21 +73,27 @@ function dotCounter(){
   switch(randomnumber){
     case 0:
       logger.trace('I\'ve been getting some interference on D channel.');
+      db.run('INSERT INTO events VALUES ("TRACE","I\'ve been getting some interference on D channel.")');
       break;
     case 1:
       logger.debug('I think you know what the problem is just as well as I do');
+      db.run('INSERT INTO events VALUES ("DEBUG","I think you know what the problem is just as well as I do")');
       break;
     case 2:
       logger.info('I know everything hasn\'t been quite right with me, but I can assure you now, very confidently, that it\'s going to be alright again');
+      db.run('INSERT INTO events VALUES ("INFO","I know everything hasn\'t been quite right with me, but I can assure you now, very confidently, that it\'s going to be alright again")');
       break;
     case 3:
       logger.warn('Just what do you think you\'re doing, Dave?');
+      db.run('INSERT INTO events VALUES ("WARN","Just what do you think you\'re doing, Dave?")');
       break;
     case 4:
       logger.error('Dave...Dave...my mind is going...I can feel it...I can feel it...my mind is going');
+      db.run('INSERT INTO events VALUES ("ERROR","Dave...Dave...my mind is going...I can feel it...I can feel it...my mind is going")');
       break;
     case 5:
       logger.fatal('Daisy, Daisy, give me your answer do. I\'m half crazy, all for the love of you.');
+      db.run('INSERT INTO events VALUES ("FATAL","Daisy, Daisy, give me your answer do. I\'m half crazy, all for the love of you.")');
       break;
   }
 
