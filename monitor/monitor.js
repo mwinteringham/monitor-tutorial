b66d45073d34f46088e2b300ad98b5fef14cec60
Mark Winteringham <mark.winteringham@digital.hmrc.gov.uk>
2015-03-17 10:09:12 +0000

    Wired in log monitor endpoint

--- monitor/monitor.js
+++ monitor/monitor.js
@@ -1,9 +1,16 @@
-var express = require('express');
+var express = require('express'),
+    grep    = require('grep1');
 
 var app = express();
 
 app.get('/log/:logtype',function(req,res){
-  res.sendStatus(200);
+  grep([req.params.logtype,'-c','../app/logs/eventgenerator.log'],function(err, stdout, stderr){
+    if (err || stderr) {
+      res.sendStatus(404);
+    } else {
+      res.status(200).send(stdout);
+    }
+  })
 })
 
 app.get('/db/:term',function(req,res){
@@ -11,5 +18,5 @@ app.get('/db/:term',function(req,res){
 })
 
 app.listen(8000,function(err){
-	if(err) return console.error(err);
+  if(err) return console.error(err);
 });
